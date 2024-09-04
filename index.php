ed7f58b149b37f804336a3ed0631bbf6c4c58668
gehan35 <115164221+gehan35@users.noreply.github.com>
2024-09-05 01:37:48 +0800

    Update: New index taskbard title

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VPRDEgital</title>
+    <title>VPRDEgital - Login</title>
     <link rel="stylesheet" href="includes/lineicons/web-font-files/lineicons.css">
     <link rel="stylesheet" href="includes/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="includes/font/poppins.css">
